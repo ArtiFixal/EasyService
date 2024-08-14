e5be32113b06f52540337c44f79b1554085bee3d
ArtiFixal <89784139+ArtiFixal@users.noreply.github.com>
2024-08-14 15:02:17 +0200

    Add max length constraint

--- easyservice-backend/src/main/java/artifixal/easyservice/dtos/DeviceDTO.java
+++ easyservice-backend/src/main/java/artifixal/easyservice/dtos/DeviceDTO.java
@@ -3,6 +3,7 @@ package artifixal.easyservice.dtos;
 import jakarta.validation.constraints.NotBlank;
 import java.util.Optional;
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 
 /**
  *
@@ -14,9 +15,11 @@ public class DeviceDTO extends BaseDTO<Long>{
     public long manufacturerID;
     
     @NotBlank(message="Device name can't be blank")
+    @Length(max=60,message="Device name can't exceed 40 characters")
     public String name;
     
     @NotBlank(message="Device serial number can't be blank")
+    @Length(max=60,message="Device serial number can't exceed 40 characters")
     public String serialNumber;
 
     public DeviceDTO(Optional<Long> id,long manufacturerID,String name,String serialNumber){
